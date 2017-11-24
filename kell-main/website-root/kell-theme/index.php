7b6a9b71562985e28768ea1a8560356eaad15fe0
Andrew Wightman <andrew@moreiradevelopment.io>
2017-11-24 15:12:45 -0500

    Scaffoling & Skinning for KELL
    - Initial Scaffolding & Skinning for KELL

---
+++ kell-main/website-root/kell-theme/index.php
@@ -0,0 +1,161 @@
+<?php include 'header.php'; ?>
+
+	<main role="main">
+		<section class="hero">
+      <h1 class="u-uppercase">Welcome to<br />100 Kellogg Lane</h1>
+      <h5>in Old East Village</h5>
+		</section>
+    
+    <section class="attraction-tiles">
+      <div class="mdev-main-wrapper">
+        <div class="heading-wrapper">
+          <h6>Lorem Ipsum</h6>
+          <h2 class="u-uppercase kell-gold">Attractions &amp; Services</h2>
+        </div>
+        <div class="tile-wrapper">
+          <div class="tile">
+            <h4>Shopping</h4>
+          </div>
+          <div class="tile">
+            <h4>Dining</h4>
+          </div>
+          <div class="tile">
+            <h4>Attractions</h4>
+          </div>
+          <div class="tile">
+            <h4>Tenant Stories</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="info-links">
+      <div class="mdev-main-wrapper">
+        <div>
+          <p class="u-uppercase"><a href="#" class="kell-grey"><i class="info-icon"></i>Visitor Information</a></p>
+        </div>
+        <div>
+          <p class="u-uppercase"><a href="#" class="kell-grey"><i class="map-icon"></i>Facility Map</a></p>
+        </div>
+        <div>
+          <p class="u-uppercase"><a href="#" class="kell-grey"><i class="map-icon"></i>About Us</a></p>
+        </div>
+        <div>
+          <p class="u-uppercase"><a href="#" class="kell-grey"><i class="document-icon"></i>Leasing</a></p>
+        </div>
+      </div>
+    </section>
+    <section class="preview-tiles preview-tile-left">
+      <div class="mdev-main-wrapper">
+        <div class="preview-tile-content">
+          <p class="sub-text u-uppercase">Canada's Biggest Indoor Adventure Park</p>
+          <h3 class="u-uppercase">The Factory</h3>
+          <p>The Factory is a 170,000 sq. ft. hub of today's most exciting and adventurous activities.  Soon to be home to North America's largest indoor ropes course, The Factory features ziplines, a trampoline park, electric indoor go-karts, escape rooms, virtual reality rooms, an arcade and a kid and toddler-friendly soft play area.</p>
+          <p class="link-wrapper">
+            <a href="#">View Details</a>
+          </p>
+        </div>
+        <div class="preview-tile-image">
+        </div>
+      </div>
+    </section>
+    <section class="preview-tiles preview-tile-right">
+      <div class="mdev-main-wrapper">
+        <div class="preview-tile-image">
+        </div>
+        <div class="preview-tile-content">
+          <p class="sub-text u-uppercase">Year-Round Entertainment</p>
+          <h3 class="u-uppercase">Courtyard</h3>
+          <p>The Courtyard will be the pulse of the 100 Kellogg community, created as a central gathering place for tenants, visitors and neighbours to enjoy.  Overlooking and accessible from the Courtyard will be Powerhouse Brewery, restaurants, shopping, a distillery, hotel, event centre, and seasonal events.</p>
+          <p class="link-wrapper">
+            <a href="#">View Details</a>
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="impact">
+      <div class="mdev-main-wrapper">
+        <div class="impact-header">
+          <div class="u-text-right">
+            <h4 class="u-uppercase kell-gold">Economic Impact:</h4>
+            <p class="sub-title u-uppercase">Tourism takes the place of<br /> corn flakes &amp; froot loops</p>
+          </div>
+          <div class="u-text-left">
+            <p>Truly iconic and unique, this multi-faceted venue is located in the heart of London in Southwestern Ontario.  The remodelled 100 Kellogg will provide office space, warehousing, shopping, dining and accommodations.  The venue will also play host to a family entertainment centre, a brewery, a distillery, concerts, banquets, weddings and more.</p>
+            <p class="link-wrapper"><a href="#" class="u-uppercase">Learn More</a></p>
+          </div>
+        </div>
+        <div class="testimonial-wrapper mdev-main-wrapper">
+          <div class="testimonial">
+            <div class="image">
+            </div>
+            <div class="content">
+              <p>"The regeneration of 100 Kellogg is truly a significant investment in the Old East Village and our city as a whole.  The contient's largest indoor ropes course, a brewery, office tower and so much more will have a significant economic impact for London.  I am confident that this will become a major destination in our city."</p>
+              <p class="author">Matt Brown</p>
+              <p class="position">Mayor, City of London</p>
+            </div>
+          </div>
+          <div class="testimonial">
+            <div class="image">
+            </div>
+            <div class="content">
+              <p>"The Factory will stimulate the local economy with new entertainment offerings while injecting incremental tourism dollars into our great city."</p>
+              <p class="author">John Winston</p>
+              <p class="position">General Manager, Tourism London</p>
+            </div>
+          </div>
+          <div class="testimonial">
+            <div class="image">
+            </div>
+            <div class="content">
+              <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla paratur."</p>
+              <p class="author">Jen Pastorius</p>
+              <p class="position">Old East Village BIA</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="phase-overview">
+      <div class="main-overview">
+        <div class="phase-heading">
+          <div class="phase-title">
+            <h3>1 Million Sq Feet of building space on 23 acres in the heart of London Ontario's Old East Village</h3>
+          </div>
+          <div class="phase-content">
+            <p class="sub-heading">This ambitious project will transform London's downtown landscape, vastly increase tourism opportunities, create jobs and stimulate the economy.</p>
+            <p>Future phases are in the works and plans are updated often as we seek out complementary tenants and businesses who wish to establish new homes at 100 Kellogg.</p>
+            <p><span class="u-bold">Phase 2 planning includes:</span> restaurants, a distillery, an event centre, Little Ray's Nature Centre, and the creation of additional office space in the building's North End.</p>
+            <p><span class="u-bold">Phase 3 planning includes:</span> hotel development, establishment of retail space and the initiation of 4th floor development.</p>
+          </div>
+        </div>
+        <div class="filmstrip">
+          <div class="image-tile"></div>
+          <div class="image-tile"></div>
+          <div class="image-tile"></div>
+          <div class="image-tile"></div>
+          <div class="image-tile"></div>
+          <div class="image-tile"></div>
+      </div>
+      <div class="overview-bottom-cta">
+        <div>
+          <p>Keep an eye on our progress!  We'll be updating these images as new areas are remodelled.</p>
+        </div>
+        <div class="button-wrapper">
+          <a href="#" class="u-uppercase">See All</a>
+        </div>
+      </div>
+      </div>
+      
+    </section>
+    <section class="logo-showcase">
+      <div class="mdev-main-wrapper">
+        <div>
+        </div>
+        <div>
+        </div>
+        <div></div>
+      </div>
+    </section>
+	</main>
+
+<?php include 'footer.php'; ?>
